e6ffc24d162443bde3e0db8450bbc5f97220e60b
Leif Etter <leif.etter@gmx.de>
2024-04-08 19:16:47 +0200

    remove unused files

--- lib/repository/constants/routing_constants.dart
+++
@@ -1,8 +0,0 @@
-const String authenticationWrapperRoute = '/wrapper';
-const String authenticationViewRoute = 'authentication';
-const String profileViewRoute = '/profile';
-const String developerViewRoute = '/developer';
-const String savedViewRoute = '/saved';
-const String createdViewRoute = '/created';
-const String contactViewRoute = '/contact';
-const String createShort = '/createShort';
--- lib/repository/constants/uri_repo.dart
+++
@@ -1,9 +0,0 @@
-class UriRepo {
-  static Uri cookieUrl = Uri.parse(
-      "https://www.iubenda.com/privacy-policy/18961044/cookie-policy");
-  static Uri dataProtectionUrl =
-      Uri.parse("https://www.iubenda.com/privacy-policy/18961044");
-
-  static Uri termsAndConditions =
-      Uri.parse("https://www.iubenda.com/nutzungsbedingungen/18961044");
-}
