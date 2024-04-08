5b91d770765b6e60a3be8af1591d240799210db8
Leif Etter <leif.etter@gmx.de>
2024-04-08 18:17:18 +0200

    add route constants into router file; add new routes

--- lib/application/routing/router.dart
+++ lib/application/routing/router.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:lebenswiki_app/presentation/screens/main_views/short_creation.dart';
-import 'package:lebenswiki_app/presentation/screens/menu_views/about_us.dart';
-import 'package:lebenswiki_app/presentation/screens/menu_views/contact.dart';
-import 'package:lebenswiki_app/presentation/screens/menu_views/created.dart';
-import 'package:lebenswiki_app/presentation/screens/menu_views/profile.dart';
-import 'package:lebenswiki_app/presentation/screens/menu_views/saved.dart';
-import 'package:lebenswiki_app/repository/constants/routing_constants.dart';
-import 'package:lebenswiki_app/main.dart';
-import 'package:lebenswiki_app/presentation/screens/other/authentication.dart';
+import 'package:lebenswiki_app/main_wrapper.dart';
+import 'package:lebenswiki_app/presentation/screens/main/short_creation.dart';
+import 'package:lebenswiki_app/presentation/screens/menu/about_us.dart';
+import 'package:lebenswiki_app/presentation/screens/menu/contact.dart';
+import 'package:lebenswiki_app/presentation/screens/menu/created.dart';
+import 'package:lebenswiki_app/presentation/screens/menu/profile.dart';
+import 'package:lebenswiki_app/presentation/screens/menu/saved.dart';
+import 'package:lebenswiki_app/presentation/screens/main/authentication.dart';
+import 'package:lebenswiki_app/presentation/screens/other/avatar_screen.dart';
+import 'package:lebenswiki_app/presentation/screens/quizzer/quiz_main.dart';
+
+const String homeRoute = '/home';
+const String authRoute = '/authentication';
+const String authRouteRegister = '/authenticationRegister';
+const String profileViewRoute = '/profile';
+const String developerViewRoute = '/developer';
+const String savedViewRoute = '/saved';
+const String createdViewRoute = '/created';
+const String createdViewRouteShorts = '/createdShorts';
+const String contactViewRoute = '/contact';
+const String createShort = '/createShort';
+const String tokenCheck = '/tokenCheck';
+const String avatarScreen = '/avatar';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
-    case authenticationWrapperRoute:
+    // case tokenCheck:
+    //   return MaterialPageRoute(
+    //     settings: settings,
+    //     builder: (context) => const AuthWrapper(),
+    //   );
+    case homeRoute:
       return PageRouteBuilder(
         settings: settings,
         pageBuilder: (context, animation, secondaryAnimation) =>
-            const AuthWrapper(),
+            const NavBarWrapper(),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           const begin = Offset(0.0, 1.0);
           const end = Offset.zero;
@@ -30,10 +49,17 @@ Route<dynamic> generateRoute(RouteSettings settings) {
           );
         },
       );
-    case authenticationViewRoute:
+    case "quizzer":
+      return MaterialPageRoute(builder: (context) => const Quizzer());
+    case authRoute:
       return MaterialPageRoute(
         settings: settings,
         builder: (context) => const AuthenticationView(),
+      );
+    case authRouteRegister:
+      return MaterialPageRoute(
+        settings: settings,
+        builder: (context) => const AuthenticationView(startWithRegister: true),
       );
     case contactViewRoute:
       return MaterialPageRoute(
@@ -60,15 +86,25 @@ Route<dynamic> generateRoute(RouteSettings settings) {
         settings: settings,
         builder: (context) => const CreatedView(),
       );
+    case createdViewRouteShorts:
+      return MaterialPageRoute(
+        settings: settings,
+        builder: (context) => const CreatedView(startWithShorts: true),
+      );
     case createShort:
       return MaterialPageRoute(
         settings: settings,
         builder: (context) => const ShortCreationView(),
       );
+    case avatarScreen:
+      return MaterialPageRoute(
+        settings: settings,
+        builder: (context) => const AvatarScreen(),
+      );
     default:
       return MaterialPageRoute(
         settings: settings,
-        builder: (context) => const AuthWrapper(),
+        builder: (context) => const NavBarWrapper(),
       );
   }
 }
