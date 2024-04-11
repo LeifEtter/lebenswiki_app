10d498bdfa168692941111a311983d4763e9c0c1
Leif Etter <leif.etter@gmx.de>
2024-04-11 18:40:29 +0200

    generate missing part files after change in models

--- lib/domain/models/pack/pack_page.model.g.dart
+++ lib/domain/models/pack/pack_page.model.g.dart
@@ -12,13 +12,20 @@ PackPage _$PackPageFromJson(Map<String, dynamic> json) => PackPage(
       items: (json['items'] as List<dynamic>)
           .map((e) => PackPageItem.fromJson(e as Map<String, dynamic>))
           .toList(),
+      type: $enumDecodeNullable(_$PageTypeEnumMap, json['type']),
     );
 
 Map<String, dynamic> _$PackPageToJson(PackPage instance) => <String, dynamic>{
       'id': instance.id,
       'pageNumber': instance.pageNumber,
       'items': instance.items.map((e) => e.toJson()).toList(),
+      'type': _$PageTypeEnumMap[instance.type],
     };
+
+const _$PageTypeEnumMap = {
+  PageType.quiz: 'quiz',
+  PageType.info: 'info',
+};
 
 PackPageItem _$PackPageItemFromJson(Map<String, dynamic> json) => PackPageItem(
       id: json['id'] as String,
@@ -46,6 +53,7 @@ const _$ItemTypeEnumMap = {
   ItemType.quiz: 'ItemType.quiz',
   ItemType.image: 'ItemType.image',
   ItemType.text: 'ItemType.text',
+  ItemType.question: 'ItemType.question',
 };
 
 PackPageItemContent _$PackPageItemContentFromJson(Map<String, dynamic> json) =>
