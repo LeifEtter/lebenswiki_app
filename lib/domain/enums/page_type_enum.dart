037dac3e4fc74b35b3e4a737dd976658f8096c9d
Leif Etter <leif.etter@gmx.de>
2024-04-12 17:47:42 +0200

    assign json values to page type enum

--- lib/domain/enums/page_type_enum.dart
+++ lib/domain/enums/page_type_enum.dart
@@ -1,1 +1,8 @@
-enum PageType { quiz, info }
+import 'package:json_annotation/json_annotation.dart';
+
+enum PageType {
+  @JsonValue("PageType.info")
+  info,
+  @JsonValue("PageType.quiz")
+  quiz,
+}
