4d18bfcf7a4c5c4f70c9ac7397f699581b8e3b6d
Leif Etter <leif.etter@gmx.de>
2024-04-17 21:43:08 +0200

    remove quiz question type

--- lib/domain/models/quiz.dart
+++ lib/domain/models/quiz.dart
@@ -1,17 +1,1 @@
-class QuizQuestion {
-  final String question;
-  final String correctAnswer;
-  late List<String> _allAnswers;
 
-  QuizQuestion(
-      {required this.question,
-      required this.correctAnswer,
-      required List<String> wrongAnswers}) {
-    (_allAnswers = wrongAnswers).add(correctAnswer);
-    answers.shuffle();
-  }
-
-  List<String> get answers => _allAnswers;
-
-  bool isAnswerCorrect(answer) => answer == correctAnswer;
-}
