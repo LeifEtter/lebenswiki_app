6b7a044e2bc1d1f07b441ce031c4e674ee52ddcb
Leif Etter <leif.etter@gmx.de>
2024-04-17 21:45:01 +0200

    rework quizzer screen

--- lib/presentation/screens/quizzer/quiz_main.dart
+++ lib/presentation/screens/quizzer/quiz_main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:lebenswiki_app/domain/models/custom_timer.dart';
 import 'package:lebenswiki_app/domain/models/pack/pack_page.model.dart';
 import 'package:lebenswiki_app/presentation/screens/quizzer/quiz_question.dart';
 import 'package:lebenswiki_app/presentation/screens/quizzer/quiz_start.dart';
 import 'package:lebenswiki_app/presentation/screens/quizzer/gyro_handler.dart';
+import 'package:lebenswiki_app/presentation/widgets/buttons/buttons.dart';
+
+class Question {
+  final String questionText;
+  final List<PackPageItemContent> answers;
+  late Map<GyroDirection, PackPageItemContent> directionToAnswerMap;
+
+  Question({required this.questionText, required this.answers}) {
+    answers.shuffle();
+    directionToAnswerMap = {
+      GyroDirection.top: answers[0],
+      GyroDirection.right: answers[1],
+      GyroDirection.bottom: answers[2],
+      GyroDirection.left: answers[3],
+    };
+  }
+}
 
 class Quizzer extends StatefulWidget {
   final PackPage packPage;
@@ -19,63 +35,138 @@ class Quizzer extends StatefulWidget {
 
 class _QuizzerState extends State<Quizzer> {
   GyroDirection direction = GyroDirection.none;
-  late CustomTimer customTimer;
-  late List<PackPageItem> quizQuestions;
+  int wrongTries = 0;
+  // late CustomTimer customTimer;
   late GyroHandler gyroHandler;
-  late PackPageItem currentQuestion;
-  String centerText = "Frage";
-  late Map<GyroDirection, PackPageItemContent> directionAnswerMap;
-  int currentIndex = 0;
+  String centerText = "";
+  late List<Question> questions;
   int timeLeft = 5;
+  final PageController _pageController = PageController(initialPage: 0);
 
   @override
   void initState() {
-    quizQuestions = widget.packPage.items
+    List<PackPageItem> onlyQuestionItems = widget.packPage.items
         .where((item) => item.type == ItemType.question)
         .toList();
+    questions = onlyQuestionItems
+        .map((PackPageItem item) => Question(
+              questionText: item.headContent.value,
+              answers: item.bodyContent,
+            ))
+        .toList();
     gyroHandler = GyroHandler(
-        updateDirectionCallback: (GyroDirection newDirection) {
-          setState(() => direction = newDirection);
-          answerEvent(newDirection);
-        },
-        timeBetweenDetections: 2);
+      updateDirectionCallback: (GyroDirection newDirection) {
+        setState(() => direction = newDirection);
+        answerEvent(newDirection);
+      },
+      timeBetweenDetections: 2,
+    );
     super.initState();
-  }
-
-  void changeQuestion(int newIndex) {
-    print(newIndex);
-    if (newIndex > quizQuestions.length + 1) {
-      print("Quiz done");
-      return;
-    }
-    PackPageItem newQuestion = quizQuestions[newIndex];
-    setState(() {
-      currentIndex = newIndex;
-      currentQuestion = newQuestion;
-      centerText = newQuestion.headContent.value;
-      directionAnswerMap = {
-        GyroDirection.top: newQuestion.bodyContent[0],
-        GyroDirection.right: newQuestion.bodyContent[1],
-        GyroDirection.bottom: newQuestion.bodyContent[2],
-        GyroDirection.left: newQuestion.bodyContent[3],
-      };
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    currentQuestion = quizQuestions[currentIndex];
     return Scaffold(
-      body: currentIndex == 0
-          ? StartPage(
-              nextPage: () => changeQuestion(1),
-            )
-          : QuizPageNew(
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        children: [
+          StartPage(
+              title: widget.packPage.items.first.headContent.value,
+              nextPage: () => nextQuestion()),
+          ...questions.map(
+            (Question question) => QuizPageNew(
               centerText: centerText,
               direction: direction,
-              directionAnswers: directionAnswerMap,
+              directionAnswers: question.directionToAnswerMap,
             ),
+          ),
+          endPage(),
+        ],
+      ),
     );
+  }
+
+  Widget endPage() => Column(
+        children: [
+          Padding(
+            padding:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.25),
+            child: SizedBox(
+              width: 400,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  starIcon(true, 80.0),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: starIcon(wrongTries < 5, 100.0),
+                  ),
+                  starIcon(wrongTries < 2, 80.0)
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text("Klasse!",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 35,
+                color: Colors.black87,
+              )),
+          const SizedBox(height: 50),
+          SizedBox(
+            width: 500,
+            child: Column(
+              children: [
+                SizedBox(
+                  width: 230,
+                  child:
+                      const LWButtons().purpleButton("Ich will nochmal!", () {
+                    setState(() {
+                      direction = GyroDirection.none;
+                      wrongTries = 0;
+                    });
+                    _pageController.jumpToPage(0);
+                  }),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 230,
+                  child: const LWButtons().outlineButton(
+                      "Zurück zum Pack", () => Navigator.pop(context)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+
+  Widget starIcon(bool filled, double size) => Icon(
+        filled ? Icons.star_rounded : Icons.star_border_rounded,
+        size: size,
+        color: Colors.yellow,
+      );
+
+  void nextQuestion() {
+    int nextPageIndex = _pageController.page!.toInt() + 1;
+    bool endNotReached = nextPageIndex <= questions.length;
+    if (endNotReached) {
+      setState(() {
+        direction = GyroDirection.none;
+        centerText = questions[nextPageIndex - 1].questionText;
+      });
+    }
+    _pageController.nextPage(
+        duration: const Duration(milliseconds: 200), curve: Curves.easeInOut);
+  }
+
+  void resetQuestion() {
+    setState(() {
+      direction = GyroDirection.none;
+      centerText = questions[_pageController.page!.toInt() - 1].questionText;
+    });
   }
 
   void answerEvent(GyroDirection newDirection) async {
@@ -83,66 +174,62 @@ class _QuizzerState extends State<Quizzer> {
     if (newDirection == GyroDirection.none) {
       return;
     }
-    if (directionAnswerMap[newDirection]!.isCorrectAnswer != null &&
-        directionAnswerMap[newDirection]!.isCorrectAnswer == true) {
-      setState(() => centerText = "Bravo!");
-      await Future.delayed(const Duration(milliseconds: (2000)));
-      changeQuestion(currentIndex += 1);
+    Question question = questions[_pageController.page!.toInt() - 1];
+    if (question.directionToAnswerMap[newDirection]!.isCorrectAnswer == true) {
+      setState(() {
+        centerText = "Bravo!";
+      });
+      await Future.delayed(const Duration(milliseconds: (1000)));
+      nextQuestion();
     } else {
       setState(() {
         centerText = "Versuchs Nochmal";
+        wrongTries++;
       });
-      await Future.delayed(const Duration(milliseconds: 2000));
-      setState(() {
-        centerText = currentQuestion.headContent.value;
-      });
+      await Future.delayed(const Duration(milliseconds: 1000));
+      resetQuestion();
     }
   }
 
-  // void onCorrectAnswer(int duration) {
-  //   pageController.nextPage(
-  //       duration: const Duration(milliseconds: 200), curve: Curves.easeInOut);
-  // }
+  // Widget _buildTimer() => Column(
+  //       children: [
+  //         Center(
+  //           child: SizedBox(
+  //             width: 100,
+  //             height: 100,
+  //             child: CircularProgressIndicator(
+  //               strokeWidth: 5,
+  //               value: 100 / 50000 * timeLeft,
+  //               strokeAlign: CircularProgressIndicator.strokeAlignInside,
+  //               backgroundColor: Colors.purple,
+  //             ),
+  //           ),
+  //         ),
+  //         Padding(
+  //           padding: const EdgeInsets.only(top: 50),
+  //           child: TextButton(
+  //             child: const Text("Toggle Timer"),
+  //             onPressed: () {
+  //               // timer =
+  //               //     Timer.periodic(const Duration(milliseconds: 10), (timer) {
+  //               //   if (timeLeft == 0) {
+  //               //     timer.cancel();
+  //               //     return;
+  //               //   }
 
-  Widget _buildTimer() => Column(
-        children: [
-          Center(
-            child: SizedBox(
-              width: 100,
-              height: 100,
-              child: CircularProgressIndicator(
-                strokeWidth: 5,
-                value: 100 / 50000 * timeLeft,
-                strokeAlign: CircularProgressIndicator.strokeAlignInside,
-                backgroundColor: Colors.purple,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 50),
-            child: TextButton(
-              child: const Text("Toggle Timer"),
-              onPressed: () {
-                // timer =
-                //     Timer.periodic(const Duration(milliseconds: 10), (timer) {
-                //   if (timeLeft == 0) {
-                //     timer.cancel();
-                //     return;
-                //   }
+  //               //   setState(() {
+  //               //     timeLeft--;
+  //               //     totalTimeSpent += 10;
+  //               //   });
+  //               // });
+  //               // timer =
+  //               //     Timer(Duration(milliseconds: 5000), () => {print("done")});
 
-                //   setState(() {
-                //     timeLeft--;
-                //     totalTimeSpent += 10;
-                //   });
-                // });
-                // timer =
-                //     Timer(Duration(milliseconds: 5000), () => {print("done")});
-
-                print("Toggling timer");
-                // quizTimer.isRunning ? quizTimer.stop() : quizTimer.start();
-              },
-            ),
-          ),
-        ],
-      );
+  //               print("Toggling timer");
+  //               // quizTimer.isRunning ? quizTimer.stop() : quizTimer.start();
+  //             },
+  //           ),
+  //         ),
+  //       ],
+  //     );
 }
--- lib/presentation/screens/quizzer/quiz_question.dart
+++ lib/presentation/screens/quizzer/quiz_question.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lebenswiki_app/domain/models/pack/pack_page.model.dart';
+import 'package:lebenswiki_app/presentation/constants/shadows.dart';
 import 'package:lebenswiki_app/presentation/screens/quizzer/gyro_handler.dart';
+import 'package:lebenswiki_app/presentation/screens/quizzer/gyro_util.dart';
 
 class QuizPageNew extends StatefulWidget {
   final String centerText;
@@ -27,11 +29,12 @@ class _QuizPageNewState extends State<QuizPageNew> {
           Align(
             alignment: Alignment.center,
             child: SizedBox(
-              width: MediaQuery.sizeOf(context).width * 0.8,
+              width: MediaQuery.sizeOf(context).width * 0.65,
               child: Text(
                 widget.centerText,
                 textAlign: TextAlign.center,
                 style: const TextStyle(
+                  fontSize: 20,
                   fontWeight: FontWeight.w500,
                 ),
               ),
@@ -46,8 +49,8 @@ class _QuizPageNewState extends State<QuizPageNew> {
               widget.direction == answerDirection
                   ? (answer.isCorrectAnswer != null &&
                           answer.isCorrectAnswer == true
-                      ? Colors.green
-                      : Colors.red)
+                      ? const Color.fromRGBO(122, 255, 127, 1.0)
+                      : const Color.fromRGBO(250, 112, 112, 1.0))
                   : null,
             );
           }),
@@ -58,45 +61,50 @@ class _QuizPageNewState extends State<QuizPageNew> {
 
   Widget _buildAnswerBox(
       GyroDirection direction, String answer, Color? backgroundColor) {
-    AlignmentGeometry alignment;
-    if (direction == GyroDirection.top) {
-      alignment = Alignment.topCenter;
-    } else if (direction == GyroDirection.bottom) {
-      alignment = Alignment.bottomCenter;
-    } else if (direction == GyroDirection.left) {
-      alignment = Alignment.centerLeft;
-    } else {
-      alignment = Alignment.centerRight;
-    }
     return Align(
-      alignment: alignment,
+      alignment: gyroDirectionToAlignment(direction),
       child: Container(
-        width:
-            direction == GyroDirection.top || direction == GyroDirection.bottom
-                ? 400
-                : 50,
-        height:
-            direction == GyroDirection.top || direction == GyroDirection.bottom
-                ? 50
-                : 400,
+        decoration: BoxDecoration(
+          color: const Color.fromRGBO(242, 232, 254, 1.0),
+          borderRadius: computeBordeRadius(direction, 20.0),
+          boxShadow: [LebenswikiShadows.commentCardShadow],
+        ),
+        width: gyroDirectionIsHorizontal(direction) ? 50 : 300,
+        height: gyroDirectionIsHorizontal(direction) ? 300 : 50,
         child: Stack(
           children: [
             Positioned.fill(
                 child: Container(
-              decoration: BoxDecoration(color: backgroundColor),
+              decoration: BoxDecoration(
+                color: backgroundColor,
+                borderRadius: computeBordeRadius(direction, 20.0),
+              ),
             )),
             Align(
               child: RotatedBox(
-                quarterTurns: direction == GyroDirection.top ||
-                        direction == GyroDirection.bottom
-                    ? 0
-                    : 1,
-                child: Text(answer),
+                quarterTurns: gyroDirectionIsHorizontal(direction) ? 1 : 0,
+                child: Text(
+                  answer,
+                  style: const TextStyle(fontWeight: FontWeight.w400),
+                ),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  BorderRadius computeBordeRadius(GyroDirection dir, double radAmount) {
+    Radius rad = Radius.circular(radAmount);
+    if (dir == GyroDirection.top) {
+      return BorderRadius.only(bottomLeft: rad, bottomRight: rad);
+    } else if (dir == GyroDirection.right) {
+      return BorderRadius.only(topLeft: rad, bottomLeft: rad);
+    } else if (dir == GyroDirection.bottom) {
+      return BorderRadius.only(topLeft: rad, topRight: rad);
+    } else {
+      return BorderRadius.only(topRight: rad, bottomRight: rad);
+    }
   }
 }
