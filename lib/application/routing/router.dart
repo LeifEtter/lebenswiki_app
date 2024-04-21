ff3ec425adcf76f309728981f80bf05c5637c2ec
Leif Etter <leif.etter@gmx.de>
2024-04-21 15:00:34 +0200

    extract go router create function to seperate router file, replacing old router

--- lib/application/routing/router.dart
+++ lib/application/routing/router.dart
@@ -1,102 +1,112 @@
-import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lebenswiki_app/domain/models/pack/pack.model.dart';
 import 'package:lebenswiki_app/main_wrapper.dart';
+import 'package:lebenswiki_app/presentation/screens/creator/creator.dart';
+import 'package:lebenswiki_app/presentation/screens/creator/creator_information.dart';
+import 'package:lebenswiki_app/presentation/screens/main/authentication.dart';
 import 'package:lebenswiki_app/presentation/screens/main/short_creation.dart';
 import 'package:lebenswiki_app/presentation/screens/menu/about_us.dart';
 import 'package:lebenswiki_app/presentation/screens/menu/contact.dart';
 import 'package:lebenswiki_app/presentation/screens/menu/created.dart';
 import 'package:lebenswiki_app/presentation/screens/menu/profile.dart';
 import 'package:lebenswiki_app/presentation/screens/menu/saved.dart';
-import 'package:lebenswiki_app/presentation/screens/main/authentication.dart';
 import 'package:lebenswiki_app/presentation/screens/other/avatar_screen.dart';
+import 'package:lebenswiki_app/presentation/screens/quizzer/quiz_main.dart';
+import 'package:lebenswiki_app/presentation/screens/viewer/view_pack.dart';
+import 'package:lebenswiki_app/presentation/screens/viewer/view_pack_started.dart';
 
-const String homeRoute = '/home';
-const String authRoute = '/authentication';
-const String authRouteRegister = '/authenticationRegister';
-const String profileViewRoute = '/profile';
-const String developerViewRoute = '/developer';
-const String savedViewRoute = '/saved';
-const String createdViewRoute = '/created';
-const String createdViewRouteShorts = '/createdShorts';
-const String contactViewRoute = '/contact';
-const String createShort = '/createShort';
-const String tokenCheck = '/tokenCheck';
-const String avatarScreen = '/avatar';
-
-Route<dynamic> generateRoute(RouteSettings settings) {
-  switch (settings.name) {
-    case homeRoute:
-      return PageRouteBuilder(
-        settings: settings,
-        pageBuilder: (context, animation, secondaryAnimation) =>
-            const NavBarWrapper(),
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          const begin = Offset(0.0, 1.0);
-          const end = Offset.zero;
-          const curve = Curves.ease;
-
-          var tween =
-              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-          return SlideTransition(
-            position: animation.drive(tween),
-            child: child,
-          );
-        },
-      );
-    case authRoute:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const AuthenticationView(),
-      );
-    case authRouteRegister:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const AuthenticationView(startWithRegister: true),
-      );
-    case contactViewRoute:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const ContactView(),
-      );
-    case developerViewRoute:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const AboutUsView(),
-      );
-    case profileViewRoute:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const ProfileView(),
-      );
-    case savedViewRoute:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const SavedView(),
-      );
-    case createdViewRoute:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const CreatedView(),
-      );
-    case createdViewRouteShorts:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const CreatedView(startWithShorts: true),
-      );
-    case createShort:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const ShortCreationView(),
-      );
-    case avatarScreen:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const AvatarScreen(),
-      );
-    default:
-      return MaterialPageRoute(
-        settings: settings,
-        builder: (context) => const NavBarWrapper(),
-      );
-  }
-}
+GoRouter createRouter(String? initialLocation) => GoRouter(
+      initialLocation: initialLocation ?? "/",
+      routes: [
+        GoRoute(
+          path: "/",
+          builder: (context, state) => const NavBarWrapper(),
+          routes: [
+            GoRoute(
+              path: "pack/overview/:id",
+              builder: (context, state) => ViewPack(
+                packId: int.parse(state.pathParameters["id"]!),
+                heroName: "",
+              ),
+            ),
+            GoRoute(
+              path: "pack/:id",
+              builder: (context, state) => PackViewerStarted(
+                packId: int.parse(state.pathParameters["id"]!),
+                heroName: "",
+              ),
+              routes: [
+                GoRoute(
+                    path: "quiz/:quizId",
+                    builder: (context, state) =>
+                        Quizzer(quizId: state.pathParameters["quizId"]!))
+              ],
+            ),
+            GoRoute(
+              path: "contact",
+              builder: (context, state) => const ContactView(),
+            ),
+            GoRoute(
+              path: "impressum",
+              builder: (context, state) => const AboutUsView(),
+            ),
+            GoRoute(
+              path: "saved",
+              builder: (context, state) => const SavedView(),
+            ),
+            GoRoute(
+              path: "created",
+              builder: (context, state) => const CreatedView(),
+              routes: [
+                GoRoute(
+                  path: "shorts",
+                  builder: (context, state) =>
+                      const CreatedView(startWithShorts: true),
+                )
+              ],
+            ),
+            GoRoute(
+              path: "profile",
+              builder: (context, state) => const ProfileView(),
+            ),
+            GoRoute(
+              path: "avatar",
+              builder: (context, state) => const AvatarScreen(),
+            ),
+            GoRoute(
+              path: "create",
+              builder: (context, state) => const CreatedView(),
+              routes: [
+                GoRoute(
+                  path: "short",
+                  builder: (context, state) => const ShortCreationView(),
+                ),
+                GoRoute(
+                  path: "pack/pages",
+                  builder: (context, state) {
+                    Pack? pack = state.extra as Pack;
+                    return Creator(pack: pack);
+                  },
+                ),
+                GoRoute(
+                  path: "pack",
+                  builder: (context, state) {
+                    Pack? pack = state.extra as Pack;
+                    return CreatorPackInfo(pack: pack);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+        GoRoute(
+          path: "/login",
+          builder: (context, state) => const AuthenticationView(),
+        ),
+        GoRoute(
+          path: "/register",
+          builder: (context, state) =>
+              const AuthenticationView(startWithRegister: true),
+        ),
+      ],
+    );
--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,13 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lebenswiki_app/application/auth/prefs_handler.dart';
+import 'package:lebenswiki_app/application/routing/router.dart';
 
 import 'package:lebenswiki_app/data/category_api.dart';
 import 'package:lebenswiki_app/domain/models/category.model.dart';
 import 'package:lebenswiki_app/domain/models/error.model.dart';
-import 'package:lebenswiki_app/domain/models/pack/pack.model.dart';
 import 'package:lebenswiki_app/domain/models/user/user.model.dart';
-import 'package:lebenswiki_app/main_wrapper.dart';
 import 'package:lebenswiki_app/presentation/providers/providers.dart';
-import 'package:lebenswiki_app/presentation/screens/creator/creator.dart';
-import 'package:lebenswiki_app/presentation/screens/creator/creator_information.dart';
-import 'package:lebenswiki_app/presentation/screens/main/authentication.dart';
-import 'package:lebenswiki_app/presentation/screens/main/short_creation.dart';
-import 'package:lebenswiki_app/presentation/screens/menu/about_us.dart';
-import 'package:lebenswiki_app/presentation/screens/menu/contact.dart';
-import 'package:lebenswiki_app/presentation/screens/menu/created.dart';
-import 'package:lebenswiki_app/presentation/screens/menu/profile.dart';
-import 'package:lebenswiki_app/presentation/screens/menu/saved.dart';
-import 'package:lebenswiki_app/presentation/screens/other/avatar_screen.dart';
-import 'package:lebenswiki_app/presentation/screens/quizzer/quiz_main.dart';
-import 'package:lebenswiki_app/presentation/screens/viewer/view_pack.dart';
-import 'package:lebenswiki_app/presentation/screens/viewer/view_pack_started.dart';
 import 'package:lebenswiki_app/presentation/widgets/common/theme.dart';
 import 'package:lebenswiki_app/data/user_api.dart';
 import 'package:lebenswiki_app/application/auth/token_handler.dart';
@@ -78,102 +64,6 @@ Future<(String, User?)> getRoute() async {
   }
   return ("/login", null);
 }
-
-GoRouter createRouter(String? initialLocation) => GoRouter(
-      initialLocation: initialLocation ?? "/",
-      routes: [
-        GoRoute(
-          path: "/",
-          builder: (context, state) => const NavBarWrapper(),
-          routes: [
-            GoRoute(
-              path: "pack/overview/:id",
-              builder: (context, state) => ViewPack(
-                packId: int.parse(state.pathParameters["id"]!),
-                heroName: "",
-              ),
-            ),
-            GoRoute(
-              path: "pack/:id",
-              builder: (context, state) => PackViewerStarted(
-                packId: int.parse(state.pathParameters["id"]!),
-                heroName: "",
-              ),
-              routes: [
-                GoRoute(
-                    path: "quiz/:quizId",
-                    builder: (context, state) =>
-                        Quizzer(quizId: state.pathParameters["quizId"]!))
-              ],
-            ),
-            GoRoute(
-              path: "contact",
-              builder: (context, state) => const ContactView(),
-            ),
-            GoRoute(
-              path: "impressum",
-              builder: (context, state) => const AboutUsView(),
-            ),
-            GoRoute(
-              path: "saved",
-              builder: (context, state) => const SavedView(),
-            ),
-            GoRoute(
-              path: "created",
-              builder: (context, state) => const CreatedView(),
-              routes: [
-                GoRoute(
-                  path: "shorts",
-                  builder: (context, state) =>
-                      const CreatedView(startWithShorts: true),
-                )
-              ],
-            ),
-            GoRoute(
-              path: "profile",
-              builder: (context, state) => const ProfileView(),
-            ),
-            GoRoute(
-              path: "avatar",
-              builder: (context, state) => const AvatarScreen(),
-            ),
-            GoRoute(
-              path: "create",
-              builder: (context, state) => const CreatedView(),
-              routes: [
-                GoRoute(
-                  path: "short",
-                  builder: (context, state) => const ShortCreationView(),
-                ),
-                GoRoute(
-                  path: "pack/pages",
-                  builder: (context, state) {
-                    Pack? pack = state.extra as Pack;
-                    return Creator(pack: pack);
-                  },
-                ),
-                GoRoute(
-                  path: "pack",
-                  builder: (context, state) {
-                    Pack? pack = state.extra as Pack;
-                    return CreatorPackInfo(pack: pack);
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
-        GoRoute(
-          path: "/login",
-          builder: (context, state) => const AuthenticationView(),
-        ),
-        GoRoute(
-          path: "/register",
-          builder: (context, state) =>
-              const AuthenticationView(startWithRegister: true),
-        ),
-      ],
-    );
 
 class LebenswikiApp extends ConsumerWidget {
   final User? user;
