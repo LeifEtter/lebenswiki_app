a5bcc57de0b215ec471a2f41f24b6a57135b3437
Leif Etter <leif.etter@gmx.de>
2024-04-26 00:02:38 +0200

    comment out unused comments code

--- lib/data/comment_api.dart
+++ lib/data/comment_api.dart
@@ -5,7 +5,6 @@ import 'package:lebenswiki_app/domain/enums/result_type_enum.dart';
 import 'package:lebenswiki_app/domain/models/error.model.dart';
 import 'package:lebenswiki_app/data/base_api.dart';
 import 'package:lebenswiki_app/data/error_handler.dart';
-import 'package:lebenswiki_app/data/result_model_api.dart';
 
 class CommentApi extends BaseApi {
   late ApiErrorHandler apiErrorHandler;
@@ -80,64 +79,65 @@ class CommentApi extends BaseApi {
   //   }
   // }
 
-  Future<ResultModel> upvoteComment(id) => _interactComment(
-      endpoint: "comment/upvote/$id",
-      successMessage: "Kommentar bewertet",
-      errorMessage: "Kommentar konnte nicht bewertet werden");
+//   Future<ResultModel> upvoteComment(id) => _interactComment(
+//       endpoint: "comment/upvote/$id",
+//       successMessage: "Kommentar bewertet",
+//       errorMessage: "Kommentar konnte nicht bewertet werden");
 
-  Future<ResultModel> downvoteComment(id) => _interactComment(
-      endpoint: "comment/downvote/$id",
-      successMessage: "Kommentar bewertet",
-      errorMessage: "Kommentar konnte nicht bewertet werden");
+//   Future<ResultModel> downvoteComment(id) => _interactComment(
+//       endpoint: "comment/downvote/$id",
+//       successMessage: "Kommentar bewertet",
+//       errorMessage: "Kommentar konnte nicht bewertet werden");
 
-  Future<ResultModel> removeUpvoteComment(id) => _interactComment(
-      endpoint: "comment/vote/remove",
-      successMessage: "Bewertung wurde entfernt",
-      errorMessage: "Bewertung konnte nicht entfernt werden");
+//   Future<ResultModel> removeUpvoteComment(id) => _interactComment(
+//       endpoint: "comment/vote/remove",
+//       successMessage: "Bewertung wurde entfernt",
+//       errorMessage: "Bewertung konnte nicht entfernt werden");
 
-  Future<ResultModel> removeDownvoteComment(id) => _interactComment(
-      endpoint: "comments/vote/remove",
-      successMessage: "Bewertung wurde entfernt",
-      errorMessage: "Bewertung konnte nicht entfernt werden");
+//   Future<ResultModel> removeDownvoteComment(id) => _interactComment(
+//       endpoint: "comments/vote/remove",
+//       successMessage: "Bewertung wurde entfernt",
+//       errorMessage: "Bewertung konnte nicht entfernt werden");
 
-  Future<ResultModel> _interactComment({
-    required String endpoint,
-    required String successMessage,
-    required String errorMessage,
-  }) async {
-    ResultModel result = ResultModel(type: ResultType.failure);
-    await patch(
-      Uri.parse("$serverIp/$endpoint"),
-      headers: await requestHeader(),
-    ).then((Response res) {
-      if (statusIsSuccess(res.statusCode)) {
-        result = ResultModel(
-          type: ResultType.success,
-          message: successMessage,
-        );
-      } else {
-        apiErrorHandler.handleAndLog(
-            responseData: jsonDecode(res.body), trace: StackTrace.current);
-      }
-    }).catchError((error) {
-      apiErrorHandler.handleAndLog(
-          responseData: error, trace: StackTrace.current);
-    });
-    return result;
-  }
+//   Future<ResultModel> _interactComment({
+//     required String endpoint,
+//     required String successMessage,
+//     required String errorMessage,
+//   }) async {
+//     ResultModel result = ResultModel(type: ResultType.failure);
+//     await patch(
+//       Uri.parse("$serverIp/$endpoint"),
+//       headers: await requestHeader(),
+//     ).then((Response res) {
+//       if (statusIsSuccess(res.statusCode)) {
+//         result = ResultModel(
+//           type: ResultType.success,
+//           message: successMessage,
+//         );
+//       } else {
+//         apiErrorHandler.handleAndLog(
+//             responseData: jsonDecode(res.body), trace: StackTrace.current);
+//       }
+//     }).catchError((error) {
+//       apiErrorHandler.handleAndLog(
+//           responseData: error, trace: StackTrace.current);
+//     });
+//     return result;
+//   }
 
-  Future<Either<CustomError, String>> deleteComment({
-    required int id,
-  }) async {
-    Response res = await delete(
-      Uri.parse("$serverIp/comment/delete/$id"),
-      headers: await requestHeader(),
-    );
-    if (statusIsSuccess(res.statusCode)) {
-      return const Right("Dein Kommentar wurde gelöscht.");
-    } else {
-      return const Left(
-          CustomError(error: "Dein Kommentar konnte nicht gelöscht werden"));
-    }
-  }
+//   Future<Either<CustomError, String>> deleteComment({
+//     required int id,
+//   }) async {
+//     Response res = await delete(
+//       Uri.parse("$serverIp/comment/delete/$id"),
+//       headers: await requestHeader(),
+//     );
+//     if (statusIsSuccess(res.statusCode)) {
+//       return const Right("Dein Kommentar wurde gelöscht.");
+//     } else {
+//       return const Left(
+//           CustomError(error: "Dein Kommentar konnte nicht gelöscht werden"));
+//     }
+//   }
+// }
 }
